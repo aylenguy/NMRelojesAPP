b78b3424a40a27528f71061a5f382f701d3d4d14
Aylen Guy <ayluguy@gmail.com>
2025-09-12 18:25:58 -0300

    cambios

--- src/pages/admin/AdminOrders.jsx
+++ src/pages/admin/AdminOrders.jsx
@@ -10,7 +10,7 @@ export default function AdminOrders() {
 
   const fetchOrders = async () => {
     try {
-      const res = await fetch(`${API_BASE_URL}/api/Venta/MyOrdersAll`, {
+      const res = await fetch(`${API_BASE_URL}/Venta/all`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
