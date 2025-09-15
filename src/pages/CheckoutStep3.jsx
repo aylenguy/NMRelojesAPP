bbfd5b68a3c13541c7734b52fb829f7fbf13685f
Aylen Guy <ayluguy@gmail.com>
2025-09-15 16:00:03 -0300

    mas cambios

--- src/pages/CheckoutStep3.jsx
+++ src/pages/CheckoutStep3.jsx
@@ -5,6 +5,7 @@ import { useCart } from "../context/CartContext";
 import { useAuth } from "../context/AuthContext";
 import { addVenta, createFromCart } from "../api/orders";
 import LogoImg from "../assets/LogoMP.png";
+import api from "../api/api";
 
 export default function CheckoutStep3() {
   const { cart, fetchCart, clearCart, loading: cartLoading } = useCart();
